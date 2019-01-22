28c3f30af5d869a1e259ac8c2a43daa6e0150721
Semen Aleksandrov <semen_aleksandrov_91@mail.ru>
2019-01-22 19:36:51 +0400

    button

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.gittest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
         button.setOnClickListener((v) -> {
             Toast.makeText(this, "Click", Toast.LENGTH_LONG).show();
+            textView.setText("Clicked");
         });
     }
 }
