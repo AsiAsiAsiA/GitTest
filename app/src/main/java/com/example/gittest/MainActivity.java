7a17dc818cda389727db55aefedec9e0c92276ed
Aleksandrov <and1-1991@mail.ru>
2019-01-23 10:41:30 +0400

    Optimize imports

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.gittest;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
